c2d9effb85fbbb1ac30d7fd1d26684f2364c982f
Trond Clemensen <trond@clemensen.no>
2021-08-07 01:02:57 +0200

    varius updates - changing computers

---
+++ chapter 4/chapter 4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace chapter_4
+{
+    class AccountTest
+    {
+        static void Main(string[] args)
+        {
+            Account myAccount = new Account();
+
+            Console.WriteLine($"Initial name is: {myAccount.GetName()}");
+
+            Console.WriteLine("Enter the new name: ");
+
+            string theName = Console.ReadLine();
+
+            myAccount.SetName(theName);
+
+            Console.WriteLine($"myAccount's name is: {myAccount.GetName}");
+        }
+    }
+
+    class Account
+    {
+        private string name;
+
+        public void SetName(string accountName)
+        {
+            name = accountName;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+    }
+}
