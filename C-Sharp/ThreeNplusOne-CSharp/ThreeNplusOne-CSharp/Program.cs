23fb24ad028f3f1541ec63d58d7f6af91cb8f7e8
Trond Clemensen <trond@clemensen.no>
2021-08-07 23:09:58 +0200

    Comments

--- C-Sharp/ThreeNplusOne-CSharp/ThreeNplusOne-CSharp/Program.cs
+++ C-Sharp/ThreeNplusOne-CSharp/ThreeNplusOne-CSharp/Program.cs
@@ -9,37 +9,39 @@ namespace ThreeNplusOne_CSharp
             Console.WriteLine("3xPlusOne counter");
 
             Console.WriteLine("Enter Start Number:");
-            int startNum = int.Parse(Console.ReadLine());
+            int startNum = int.Parse(Console.ReadLine()); // asks which number you want to start with
 
-            Console.WriteLine("Enter the desired number of iterations");
+            Console.WriteLine("Enter the desired number of iterations"); // Asks how many iterations 
             int numIterations = int.Parse(Console.ReadLine());
 
-            for (int x = startNum; x <= startNum + numIterations; x++)
+            for (int x = startNum; x <= startNum + numIterations; x++) // will loop x amount of times
             {
-                int i = DoTheCalculation(x); 
+                int i = DoTheCalculation(x); // Calls the function/method
 
-                Console.WriteLine(x + " " + i);
+                Console.WriteLine(x + " " + i); // Should be fairly self explanatory... Writes the result to the console.
             }
         }
 
-        static int DoTheCalculation(int n)
+        static int DoTheCalculation(int n) 
+            // This function/method will do the actual calculation of the 3n+1 problem, and return the number of iterations 
+            // reached before reaching the final loop (1 2 4). 
         {
             int i = 0;
-            while (n != 1)
+            while (n != 1)  // Checks to see if final loop is reached (if n=1, we know we reached the end loop)
                 {
-                    if (n % 2 == 0)
+                    if (n % 2 == 0) // checks to see if number is even
                     {
-                        n = n / 2;
+                        n = n / 2;  // If number is even, divide by 2
                     }
                     else
                     {
-                        n = (n * 3) + 1;
+                        n = (n * 3) + 1; // if number is odd, multiply by 3 and add 1
                     }
 
-                i++;
+                i++; // increase iteration counter by one
                 }
 
-            return i;            
+            return i; // returns the number of iterations 
         }
     } 
 }
