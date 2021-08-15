abe20c5c7ccf0f53bca315d618a14cd5c49fbd9d
Trond Clemensen <trond@clemensen.no>
2021-08-15 21:31:26 +0200

    blrf

---
+++ C-Sharp/Random miniprojects/VSCode-Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VSCode_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
